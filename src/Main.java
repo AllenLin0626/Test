75b20f6c957eb1533e3c4bf6f20ef85a5d887e19
Allen <allenlin0626@gmail.com>
2015-11-08 21:09:33 +0800

    day 1, Review day 0 and learn English Grammar

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        //day 0
 //        String name = "Fido";
 //        System.out.print("Dog:" + name);
 
 //        int eight = 8;
 //        int nine = 9;
-
         String orangeTag = "10";//十
         String appleTag = "20";//廿
 
@@ -17,5 +17,22 @@ public class Main {
         System.out.print("total price is ");
         int num = orangePrice + appleprice;
         System.out.print(num);
+
+        //day 1
+
+//        int sizeOfHoskey=150;
+//        int sizeOfLapulado=200;
+
+//        String hoskey="Hoskey";
+//        String lapulado="Lapulado";
+
+        Dog dog1 = new Dog("hoskey", 150);
+        Dog dog2 = new Dog("lapulado", 200);
+        Dog dog3 = new Dog("veryExpensive", 50);
+
+
+        int[] numList = {2, 2, 2, 2};
+        String[] nameList = {"lalala", "bbb", "bbb"};
+
     }
 }
