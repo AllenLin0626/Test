10d28bea3b2b2a88d7826810a20acfdf2ae1c0c1
Allen <allen0626@gmail.com>
2015-11-16 00:38:04 +0800

    day 2, Starting read Head First Java and learn more Java concept.

---
+++ src/Num99to0.java
@@ -0,0 +1,16 @@
+import java.util.logging.XMLFormatter;
+
+/**
+ * Created by allen on 11/16/15.
+ */
+public class Num99to0 {
+    public static void main(String[] args) {
+        int x = 99;
+        while (x > -1) {
+            System.out.println(x);
+            x = x - 1;
+        }
+
+    }
+
+}
